c60a80455bc81a07dc593f5972240b52c514e415
Xitsembiso Baloyi <122219800+xbaloyi@users.noreply.github.com>
2024-05-15 06:16:39 +0200

    Update app.py

--- src/astt_gui/app.py
+++ src/astt_gui/app.py
@@ -86,7 +86,7 @@ def start_astt_gui():
          "button" in request.form
          and request.form["button"] == "Initialize"
      ):
-         user_pass = os.environ.get('ASTT_PASSWORD')
+        user_pass = os.environ.get('ASTT_PASSWORD')
 
     # Trigger condition when Initialize button is clicked.
     # if "button" in request.form and request.form["button"] == "Initialize":
